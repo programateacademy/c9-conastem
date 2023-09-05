f4027d474212a39982355f79f9ae159a0b16dd9c
mellamanmar <marimelissamc@gmail.com>
2023-09-05 16:59:26 -0500

    Create index template and views

--- CONASTEM/DataBase/views/INFRAESTRUCTURA/Equidad.py
+++
@@ -1,7 +0,0 @@
-from django.views import generic
-from ...models.INFRAESTRUCTURA.Equidad import Equidad
-
-class EquidadView (generic.ListView):
-    model= Equidad
-    context_object_name= 'Equidad_SubCriterio'
-    template_name= ''
