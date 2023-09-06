bce93946ebbc8bc7a7db8ea8bfadd0544ba68c2a
GynaHerrera <gynalexa0206@gmail.com>
2023-09-06 12:05:11 -0500

    modelo de persona responsable

--- CONASTEM/DataBase/models/Person_Responsable.py
+++ CONASTEM/DataBase/models/Person_Responsable.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 
+
 class PersonResponsable (models.Model):
-    pass
+   
+    firs_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    phone_number=models.IntegerField()
+    email=models.EmailField(max_length=200)
