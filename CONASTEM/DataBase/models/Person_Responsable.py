9b513dd11f65549effde25138153e858b2a5724a
GynaHerrera <gynalexa0206@gmail.com>
2023-09-06 15:22:04 -0500

    modelo de persona responsable y auditoria

--- CONASTEM/DataBase/models/Person_Responsable.py
+++ CONASTEM/DataBase/models/Person_Responsable.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class PersonResponsable (models.Model):
    
-    firs_name=models.CharField(max_length=100)
-    last_name=models.CharField(max_length=100)
-    phone_number=models.IntegerField()
-    email=models.EmailField(max_length=200)
+     first_name=models.CharField(max_length=100,default="")
+     last_name=models.CharField(max_length=100,default="")
+     phone_number=models.CharField(max_length=20,default="")
+     email=models.EmailField(max_length=200, default='example@example.com')
+
+     def __str__(self):
+        return self.last_name +" "+ self.first_name
