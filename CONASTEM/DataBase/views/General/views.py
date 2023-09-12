68d4f53a50a83aa912b55763fd9f8546cf43ccac
mellamanmar <marimelissamc@gmail.com>
2023-09-12 15:14:28 -0500

    Create forms

--- CONASTEM/DataBase/views/General/views.py
+++ CONASTEM/DataBase/views/General/views.py
@@ -10,4 +10,4 @@ from ...models.Criterio import Criterio
 class CriterioList(generic.ListView):
     model= Criterio
     context_object_name= 'criterio_list'
-    template_name= 'index.html'
+    template_name= 'criterios.html'
