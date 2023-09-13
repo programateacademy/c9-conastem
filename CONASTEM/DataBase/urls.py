0d4d4c82a1ec79f63c3d77d34471e8591ad9af69
GynaHerrera <gynalexa0206@gmail.com>
2023-09-13 17:23:50 -0500

    Vista relaciones con la comunidad

--- CONASTEM/DataBase/urls.py
+++ CONASTEM/DataBase/urls.py
@@ -6,6 +6,7 @@ from .views.Curriculo.Desarrollo_ciudadania_digital import DesarrolloCiudadaniaD
 from .views.EscuelaComunidadYPertenencia import CompromisodelaComunidadListView
 from .views.EscuelaComunidadYPertenencia import ConvivenciaescolarListView
 from .views.EscuelaComunidadYPertenencia import EscuelaComunidadyPertenenciaListView
+from .views.EscuelaComunidadYPertenencia import RelacionesconlaComunidadListView
 
 urlpatterns= [
     path('', views.CriterioList.as_view(), name= 'criterio_list'),
@@ -14,6 +15,7 @@ urlpatterns += [
     path("escuelacomunidadypertenencia", EscuelaComunidadyPertenenciaListView.as_view(),name="Escuelacomunidadypertenencia"),
     path("compromisodelacomunidad", CompromisodelaComunidadListView.as_view(), name="Compromisodelacomunidad_List"),
     path("convivenciaescolar", ConvivenciaescolarListView.as_view(), name="Convivenciaescolar_List"),
+    path("relacionesconlacomunidad", RelacionesconlaComunidadListView.as_view(), name="Relacionesconlacomunidad_List"),
 
 
     path('inclusion_ingenieria_aula/', InclusionIngenieriaAulaListView.as_view(), name='inclusion_ingenieria_aula_list'),
