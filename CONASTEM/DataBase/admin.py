477950050d0b8a1e6b56a867d6674626a93d9918
LuaMartz <luisamartinez030503@gmail.com>
2023-09-14 00:15:15 -0500

    update

--- CONASTEM/DataBase/admin.py
+++ CONASTEM/DataBase/admin.py
@@ -9,7 +9,7 @@ from .models.Person_Responsable import PersonResponsable
 from .models.Register import Register
 
 # INFRAESTRUCTURA
-from .models.INFRAESTRUCTURA.Equidad import Equidad 
+from .models.Infraestructura.Equidad import Equidad 
 
 # CURRICULO
 from .models.Curriculo.Inclusion_ingenieria_aula import Inclusion_ingenieria_aula
