5f61b11551217728d8040cfd131a8f050cf48f9c
GynaHerrera <gynalexa0206@gmail.com>
2023-09-15 16:17:03 -0500

    cambio de formato templates de escuela, comunidad y pertenencia

--- CONASTEM/DataBase/models/Escuela_Comunidad_y_Pertenencia/ConvivenciaEscolar.py
+++ CONASTEM/DataBase/models/Escuela_Comunidad_y_Pertenencia/ConvivenciaEscolar.py
@@ -5,18 +5,20 @@ from ..GeneralModel import GeneralModel
 class ConvivenciaEscolar (GeneralModel):
 
     ITEM_CHOICE = [
-        ("531","Los estudiantes se tratan con confianza y respeto."),
-        ("532","El docente facilita un ambiente positivo de aprendizaje social y emocional."),
-        ("533","El personal del colegio enfatiza en el código de conducta y los valores."),
-        ("534","El personal de la institución apoya las necesidades de todos los estudiantes."),
-        ("535","Los estudiantes contribuyen en las decisiones de la institución."),
-        ("536","Los estudiantes demuestran la aplicación del código de conducta y los valores.")
+        ("Convivencia Escolar","5.3.0.0"),
+        ("Los estudiantes se tratan con confianza y respeto.","5.3.1.0"),
+        ("El docente facilita un ambiente positivo de aprendizaje social y emocional.","5.3.2.0"),
+        ("El personal del colegio enfatiza en el código de conducta y los valores.","5.3.3.0"),
+        ("El personal de la institución apoya las necesidades de todos los estudiantes.","5.3.4.0"),
+        ("Los estudiantes contribuyen en las decisiones de la institución.","5.3.5.0"),
+        ("Los estudiantes demuestran la aplicación del código de conducta y los valores.","5.3.6.0")
     ]
 
     numeral = models.CharField(
         max_length= 10000,
         choices= ITEM_CHOICE
         )
+    codigo =models.CharField(max_length=7, default="5.3.0.0")
     
     def __str__(self) :
         return self.numeral
--- CONASTEM/DataBase/models/Escuela_Comunidad_y_Pertenencia/RelacionesconlaComunidad.py
+++ CONASTEM/DataBase/models/Escuela_Comunidad_y_Pertenencia/RelacionesconlaComunidad.py
@@ -5,14 +5,15 @@ from ..GeneralModel import GeneralModel
 class RelacionesconlaCominudad (GeneralModel):
 
     ITEM_CHOICE = [
-        ("521","Los estudiantes tienen experiencias directas con profesionales de áreas STEM en entornos auténticos."),
-        ("522","Las experiencias de campo y con profesionales invitados están integrados dentro de las actividades para aumentar el conocimiento de los estudiantes y ayudar en la selección de carrera."),
+        ("Relaciones con la Comunidad","5.2.0.0"),
+        ("Los estudiantes tienen experiencias directas con profesionales de áreas STEM en entornos auténticos.","5.2.1.0"),
+        ("Las experiencias de campo y con profesionales invitados están integrados dentro de las actividades para aumentar el conocimiento de los estudiantes y ayudar en la selección de carrera.","5.2.2.0"),
         ]
 
     numeral = models.CharField(
         max_length= 10000,
         choices= ITEM_CHOICE
         )
-    
+    codigo =models.CharField(max_length=7, default="5.2.0.0")
     def __str__(self) :
         return self.numeral
