56afdd0239cd68c8ca46a7d508e57553391298eb
Mafe Valencia <valenciamafe651@gmail.com>
2023-09-25 17:26:19 -0500

    fixing errors

--- CONASTEM/DataBase/static/script.js
+++ CONASTEM/DataBase/static/script.js
@@ -5,11 +5,8 @@ icons.addEventListener('click', function() {
     icons.style.fill = '#E51954'; // Cambia a fucsia
 });
 
-<<<<<<< HEAD
-=======
 // Agrega un event listener al SVG para detectar clics
 items.addEventListener('click', function() {
     items.style.border = "2px #000 solid";
     ; // Cambia a amarillo
 });
->>>>>>> 56f23a645bc6657735f7ffebea7b99434cb46176
